c1c6edbac24a3ec72962f44c23ac31f9e66c5155
한지수 <hanjisu@hanjisuui-MacBook-Air.local>
2017-08-12 21:58:18 +0900

    first commit

---
+++ db/migrate/20170812060848_create_items.rb
@@ -0,0 +1,13 @@
+class CreateItems < ActiveRecord::Migration[5.1]
+  def change
+    create_table :items do |t|
+      t.integer :user_id
+      t.string :name
+      t.string :category
+      t.string :sub_category
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
